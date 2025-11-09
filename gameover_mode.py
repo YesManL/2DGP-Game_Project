5525c4f7a2684f1d983bc7dab8e760c75b040776
folder <ht51566@gmail.com>
2025-11-09 18:03:24 +0900

    타이틀 화면 및 게임오버 화면 추가

---
+++ gameover_mode.py
@@ -0,0 +1,43 @@
+from pico2d import *
+import game_framework
+import title_mode
+
+def init():
+    global font, score
+    # Windows 기본 폰트 사용
+    font = load_font('C:/Windows/Fonts/arial.ttf', 40)
+    score = 0
+
+def finish():
+    pass
+
+def set_score(kills):
+    global score
+    score = kills
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
+            elif event.key == SDLK_SPACE:
+                game_framework.change_mode(title_mode)
+
+def draw():
+    clear_canvas()
+    font.draw(get_canvas_width() // 2 - 100, get_canvas_height() // 2 + 50, 'GAME OVER', (255, 0, 0))
+    font.draw(get_canvas_width() // 2 - 120, get_canvas_height() // 2 - 20, f'Kills: {score}', (255, 255, 255))
+    font.draw(get_canvas_width() // 2 - 200, get_canvas_height() // 2 - 100, 'Press SPACE to Restart', (255, 255, 0))
+    update_canvas()
+
+def update():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
---
+++ title_mode.py
@@ -0,0 +1,46 @@
+from pico2d import *
+import game_framework
+import play_mode
+
+def init():
+    global image, font
+    image = None
+    try:
+        image = load_image('./99.etc/Title2.png')
+    except:
+        pass
+    # Windows 기본 폰트 사용
+    font = load_font('C:/Windows/Fonts/arial.ttf', 40)
+
+def finish():
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
+            elif event.key == SDLK_SPACE:
+                game_framework.change_mode(play_mode)
+
+def draw():
+    clear_canvas()
+    if image:
+        image.draw(get_canvas_width() // 2, get_canvas_height() // 2)
+    else:
+        font.draw(get_canvas_width() // 2 - 150, get_canvas_height() // 2, 'SAND RAIDER', (255, 255, 255))
+
+    font.draw(get_canvas_width() // 2 - 200, get_canvas_height() // 2 - 100, 'Press SPACE to Start', (255, 255, 0))
+    update_canvas()
+
+def update():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
