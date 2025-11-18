e1e721ec7f12d2b5aa2ad26b22f81bf285d256d3
folder <ht51566@gmail.com>
2025-11-18 20:11:08 +0900

    적 전용 총알 클래스 추가 (EnemyBullet)

--- bullet.py
+++ bullet.py
@@ -57,3 +57,59 @@ class Bullet:
     def handle_collision(self, group, other):
         if group == 'bullet:enemy':
             game_world.remove_object(self)
+
+
+# 적 전용 총알 클래스
+class EnemyBullet:
+    images = None
+
+    def __init__(self, x, y, angle, speed=200, damage=5):
+        self.x, self.y = x, y
+        self.angle = angle
+        self.speed = speed
+        self.damage = damage
+        self.width, self.height = 30, 30  # 적 총알은 조금 작게
+
+        # 적 총알용 이미지 로드 (동일한 VFX 사용하되 색상이 다르게 보이도록)
+        if EnemyBullet.images is None:
+            EnemyBullet.images = []
+            for i in range(1, 8):  # 7프레임
+                frame_file = f'05.VFX/VFX_Bullet/VFX_Bullet_1/VFX_Bullet_1_{i:04d}.png'
+                EnemyBullet.images.append(load_image(frame_file))
+
+        # 애니메이션 관련
+        self.frame = 0
+        self.frame_count = 7
+        self.frame_time = 0
+        self.frame_per_action = 0.05
+
+        # 각도를 라디안으로 변환하여 방향 계산
+        rad = math.radians(angle + 90)
+        self.dir_x = math.cos(rad)
+        self.dir_y = math.sin(rad)
+
+    def update(self):
+        # 위치 업데이트
+        self.x += self.dir_x * self.speed * game_framework.frame_time
+        self.y += self.dir_y * self.speed * game_framework.frame_time
+
+        # 애니메이션 프레임 업데이트
+        self.frame_time += game_framework.frame_time
+        if self.frame_time >= self.frame_per_action:
+            self.frame = (self.frame + 1) % self.frame_count
+            self.frame_time = 0
+
+        # 화면 밖으로 나가면 삭제
+        if self.x < 0 or self.x > get_canvas_width() or self.y < 0 or self.y > get_canvas_height():
+            game_world.remove_object(self)
+
+    def draw(self):
+        # 적 총알은 빨간색 틴트를 적용 (optional)
+        EnemyBullet.images[self.frame].rotate_draw(math.radians(self.angle), self.x, self.y, self.width, self.height)
+
+    def get_bb(self):
+        return self.x - 15, self.y - 15, self.x + 15, self.y + 15
+
+    def handle_collision(self, group, other):
+        if group == 'enemy_bullet:player':
+            game_world.remove_object(self)
