f9bf81c100d24a09d6bee45910dc8470858d39e1
임호권/게임공학과/학생 <lhk0115@tukorea.ac.kr>
2025-12-02 13:26:48 +0900

    커서 변수 초기화 오류 수정 및 SDL 커서 함수로 변경

--- play_mode.py
+++ play_mode.py
@@ -168,7 +168,7 @@ def draw():
 
 def finish():
     game_world.clear()
-    show_cursor()  # 게임 종료 시 커서 다시 보이기
+    SDL_ShowCursor(SDL_ENABLE)  # 게임 종료 시 커서 다시 보이기
 
 def pause():
     global game_paused
