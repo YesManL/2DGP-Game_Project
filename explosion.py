2b698a4f2c30d8022de799636a861da09c4ec290
임호권/게임공학과/학생 <lhk0115@tukorea.ac.kr>
2025-12-03 14:30:59 +0900

    보스용 폭발탄 구현

--- explosion.py
+++ explosion.py
@@ -82,3 +82,73 @@ class Explosion:
     def handle_collision(self, group, other):
         pass
 
+
+# 보스용 폭발 (플레이어에게 데미지)
+class BossExplosion:
+    """보스 폭발탄 - 플레이어에게 광역 피해"""
+    images = None
+
+    def __init__(self, x, y, damage, radius):
+        self.x, self.y = x, y
+        self.damage = damage
+        self.radius = radius
+
+        # 폭발 애니메이션 (동일한 이미지 사용)
+        if BossExplosion.images is None:
+            BossExplosion.images = []
+            for i in range(1, 11):
+                try:
+                    img = load_image(f'./05.VFX/VFX_Explosion/VFX_Explosion_1/VFX_Explosion_1_{i:04d}.png')
+                    BossExplosion.images.append(img)
+                except:
+                    pass
+
+        self.frame = 0
+        self.frame_speed = 20
+        self.time = 0
+        self.is_dead = False
+        self.has_dealt_damage = False
+
+    def update(self):
+        self.time += game_framework.frame_time
+        self.frame = int(self.time * self.frame_speed)
+
+        # 첫 프레임에서 플레이어에게 피해
+        if not self.has_dealt_damage and self.frame >= 2:
+            self.deal_area_damage()
+            self.has_dealt_damage = True
+
+        # 애니메이션 끝나면 제거
+        if self.frame >= len(BossExplosion.images):
+            self.is_dead = True
+            game_world.remove_object(self)
+
+    def deal_area_damage(self):
+        """폭발 반경 내의 플레이어에게 피해"""
+        import play_mode
+        player = play_mode.player
+
+        if player:
+            # 플레이어와의 거리 계산
+            dx = player.x - self.x
+            dy = player.y - self.y
+            distance = (dx ** 2 + dy ** 2) ** 0.5
+
+            # 폭발 반경 내에 있으면 피해
+            if distance <= self.radius:
+                player.hp -= self.damage
+
+    def draw(self):
+        if self.frame < len(BossExplosion.images):
+            # 폭발 이펙트 크기 (반경의 2배)
+            size = self.radius * 2
+            BossExplosion.images[self.frame].draw(self.x, self.y, size, size)
+
+    def get_bb(self):
+        # 충돌 박스 (광역)
+        return self.x - self.radius, self.y - self.radius, self.x + self.radius, self.y + self.radius
+
+    def handle_collision(self, group, other):
+        # 폭발은 충돌해도 사라지지 않음 (애니메이션이 끝날 때까지 지속)
+        pass
+
